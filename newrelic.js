e7f9ebfb481feeb06dcbe564809457e52b3878b0
blakeparkinson <wizardplow@gmail.com>
2016-06-14 11:12:09 -0600

    get rid of the key

--- newrelic.js
+++ newrelic.js
@@ -14,7 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
-  license_key: 'ea83e691e01daa89d7fa2c44201532b34b4f3d76',
+  license_key: process.env.newRelicKey,
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
@@ -23,4 +23,4 @@ exports.config = {
      */
     level: 'info'
   }
-}
+};
