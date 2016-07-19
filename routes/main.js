92ecbe6669f0037f6076ca56452975229941bb49
blakeparkinson <wizardplow@gmail.com>
2016-07-19 10:10:54 -0600

    adding the ability to do markup in the emails

--- routes/main.js
+++ routes/main.js
@@ -6,53 +6,59 @@ var app = express();
 var cors = require('cors')
 
 var transporter = nodemailer.createTransport(smtpTransport({
-  service: 'Gmail',
-  auth: {
-      user: process.env.emailUsername || config.gmail_un,
-      pass: process.env.emailPassword || config.gmail_pw
-  }
+    service: 'Gmail',
+    auth: {
+        user: process.env.emailUsername || config.gmail_un,
+        pass: process.env.emailPassword || config.gmail_pw
+    }
 }));
 
-router.post('/email', cors(), function(req,res){
-  console.log(process.env);
+router.post('/email', cors(), function(req, res) {
 
+    var markup = ['<div>Firmware Version: <b>' + req.body.firmwareVersion + '</b></div>',
+        '<div>App Version: <b>' + req.body.appVersion + '</b></div>',
+        '<div style="margin-top:20px;">' + req.body.comments + '</div>'
+    ];
+    var html = markup.join('');
 
     var mailOptions = {
 
         from: req.body.firstName, // sender address
         to: 'alpinelabsemails@gmail.com', // list of receivers
         subject: req.body.firstName + ' ' + '(' + req.body.email + ')', // Subject line
-        text: req.body.comments // plaintext body
+        html: html
     };
 
-    if (req.body.attachment){
-      mailOptions.attachments = [
-        {
-          filename: 'error.log',
-          content: req.body.attachment
-        }
-      ];
-    }
-
-    else{
-      mailOptions.text += ' (No attachment was provided)';
+    if (req.body.attachment) {
+        mailOptions.attachments = [{
+            filename: 'error.log',
+            content: req.body.attachment
+        }];
+    } else {
+        mailOptions.text += ' (No attachment was provided)';
     }
 
     // send mail with defined transport object
-    transporter.sendMail(mailOptions, function(error, info){
-      console.log(mailOptions);
-        if(error){
+    transporter.sendMail(mailOptions, function(error, info) {
+        console.log(mailOptions);
+        if (error) {
             console.log(error);
-            res.json('error',{error: 'failed to send email'});
+            res.json('error', {
+                error: 'failed to send email'
+            });
 
-        }else{
-            res.json({success: true});
+        } else {
+            res.json({
+                success: true
+            });
         }
     });
 });
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 
 });
 
