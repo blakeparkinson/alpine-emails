f59069aadcebc80ead0b31f601a2c55c7ec7d33b
blakeparkinson <wizardplow@gmail.com>
2016-08-26 09:38:09 -0600

    error checking

--- routes/main.js
+++ routes/main.js
@@ -9,21 +9,10 @@ var transporter = nodemailer.createTransport(smtpTransport({
     service: 'Gmail',
     auth: {
         user: process.env.emailUsername || config.gmail_un,
-        pass: process.env.emailPassword || config.gmail_pw
+        pass: 'mehhhhh'//process.env.emailPassword || config.gmail_pw
     }
 }));
 
-router.get('/:search', cors(), function(req, res) {
-  console.log(req.params);
-  var quotes = [
-  { author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
-];
-  console.log('we made it');
-    res.json(quotes);
-});
 
 router.post('/email', cors(), function(req, res) {
 
