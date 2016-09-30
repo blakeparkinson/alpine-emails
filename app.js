0db839966bf4dbe83bc3971f4dc1f5dbb2fd3b4e
blakeparkinson <wizardplow@gmail.com>
2016-09-30 10:31:46 -0600

    increase the upload limit

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.limit('15mb'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
