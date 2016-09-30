dbbfdeb35cd5f46761a6676c66af88b0cdeb71bc
blakeparkinson <wizardplow@gmail.com>
2016-09-30 10:40:27 -0600

    setting a limit this way

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.limit('15mb'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,9 +31,13 @@ app.use('/main', main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  getRawBody(req, {
+    limit: '15mb'
+  }, function(err, string){
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  }
 });
 
 // error handlers
