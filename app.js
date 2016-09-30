a90668f4badcff8d08c0595317c695844bc72186
blakeparkinson <wizardplow@gmail.com>
2016-09-30 10:46:57 -0600

    trying to do the limit with body parser

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '15mb'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -31,13 +31,9 @@ app.use('/main', main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  getRawBody(req, {
-    limit: '15mb'
-  }, function(err, string){
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
