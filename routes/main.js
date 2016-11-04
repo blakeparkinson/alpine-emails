8dce2ddea2a07adcbc20698ae4067bee1488c6f0
blakeparkinson <wizardplow@gmail.com>
2016-11-04 15:03:52 -0600

    g

--- routes/main.js
+++ routes/main.js
@@ -12,6 +12,7 @@ var template = swig.compileFile(__dirname + '/../templates/bugreply.html');
 var plainTemplate = swig.compileFile(__dirname + '/../templates/bugreply.txt');
 
 var request = require('request');
+var querystring = require('querystring');
 
 var transporter1 = nodemailer.createTransport("SMTP", {
     service: 'gmail',
@@ -48,7 +49,7 @@ router.post('/images', cors(), function(req, res) {
       'Accept': '*/*',
       'Host': 'waifu2x.udp.jp'
     },
-    url: 'http://waifu2x.udp.jp/api', method: 'POST', body: {'noise': 1, 'scale': 2, 'style': 'photo'}, uri: 'https://s3.amazonaws.com/alpine-misc/pulse-thumb.jpg'}), function(err, httpResponse, body){
+    url: 'http://waifu2x.udp.jp/api', method: 'POST', body: querystring.stringify({'noise': 1, 'scale': 2, 'style': 'photo'}), uri: 'https://s3.amazonaws.com/alpine-misc/pulse-thumb.jpg'}), function(err, httpResponse, body){
     console.log(err);
     console.log(httpResponse);
     console.log(body);
