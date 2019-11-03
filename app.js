f87954104a96fb0278e1cf3c80a3382a854b56ad
Blake Parkinson <wizardplow@gmail.com>
2019-11-02 21:01:21 -0600

    updates

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-require('newrelic');
+var express = require('express')
+var path = require('path')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 
-var routes = require('./routes/index');
-var main = require('./routes/main');
+var routes = require('./routes/index')
+var main = require('./routes/main')
 
-
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(cors());
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+app.use(cors())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json({limit: '15mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json({ limit: '15mb' }))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/main', main);
-
+app.use('/', routes)
+app.use('/main', main)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
@@ -42,23 +39,22 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-module.exports = app;
+module.exports = app
--- routes/main.js
+++ routes/main.js
@@ -1,138 +1,128 @@
-var express = require('express');
-var nodemailer = require("nodemailer");
-var router = express.Router();
-var smtpTransport = require("nodemailer-smtp-transport");
-var smtpPool = require('nodemailer-smtp-pool');
-var app = express();
-var cors = require('cors');
-var xoauth2 = require('xoauth2');
-var wellknown = require('nodemailer-wellknown');
-var swig = require('swig');
-var template = swig.compileFile(__dirname + '/../templates/bugreply.html');
-var plainTemplate = swig.compileFile(__dirname + '/../templates/bugreply.txt');
+var express = require('express')
+var nodemailer = require('nodemailer')
+var router = express.Router()
+var smtpTransport = require('nodemailer-smtp-transport')
+var smtpPool = require('nodemailer-smtp-pool')
+var app = express()
+var cors = require('cors')
+var xoauth2 = require('xoauth2')
+var wellknown = require('nodemailer-wellknown')
+var swig = require('swig')
+var template = swig.compileFile(__dirname + '/../templates/bugreply.html')
+var plainTemplate = swig.compileFile(__dirname + '/../templates/bugreply.txt')
 
-var transporter1 = nodemailer.createTransport("SMTP", {
-    service: 'gmail',
-    auth: {
-        XOAuth2: {
-            user: "alpinelabsemails@gmail.com", // Your gmail address.
-            clientId: process.env.googleClientId,
-            clientSecret: process.env.googleClientSecret,
-            refreshToken: process.env.googleRefreshToken
-        }
+var transporter1 = nodemailer.createTransport('SMTP', {
+  service: 'gmail',
+  auth: {
+    XOAuth2: {
+      user: 'alpinelabsemails@gmail.com', // Your gmail address.
+      clientId: process.env.googleClientId,
+      clientSecret: process.env.googleClientSecret,
+      refreshToken: process.env.googleRefreshToken
     }
-});
+  }
+})
 
-var transporter2 = nodemailer.createTransport("SMTP", {
-    service: 'Godaddy',
-    auth: {
-        user: 'bug-reports@alpinelaboratories.com',
-        pass: process.env.goDaddyPw
-    }
-});
+var transporter2 = nodemailer.createTransport('SMTP', {
+  service: 'Godaddy',
+  auth: {
+    user: 'bug-reports@alpinelaboratories.com',
+    pass: process.env.goDaddyPw
+  }
+})
 
+router.post('/nonsense', cors(), function(req, res) {
+  console.log('request made to the email api')
 
-router.post('/email', cors(), function(req, res) {
+  var markup = [
+    '<div>Firmware Version: <b>' + req.body.firmwareVersion + '</b></div>',
+    '<div>App Version: <b>' + req.body.appVersion + '</b></div>',
+    '<div>Device Model: <b>' + req.body.deviceModel + '</b></div>',
+    '<div>Device Platform: <b>' + req.body.devicePlatform + '</b></div>',
+    '<div>Device OS: <b>' + req.body.deviceVersion + '</b></div>',
+    '<div>Firmware Type: <b>' + req.body.firmwareType + '</b></div>',
+    '<div style="margin-top:20px;">' + req.body.comments + '</div>'
+  ]
+  var html = markup.join('')
 
-    console.log('request made to the email api');
+  var mailOptions = {
+    from: req.body.firstName, // sender address
+    to: 'alpinelabsemails@gmail.com', // list of receivers
+    subject: req.body.firstName + ' ' + '(' + req.body.email + ')', // Subject line
+    html: html
+  }
 
-    var markup = ['<div>Firmware Version: <b>' + req.body.firmwareVersion + '</b></div>',
-        '<div>App Version: <b>' + req.body.appVersion + '</b></div>',
-        '<div>Device Model: <b>' + req.body.deviceModel + '</b></div>',
-        '<div>Device Platform: <b>' + req.body.devicePlatform + '</b></div>',
-        '<div>Device OS: <b>' + req.body.deviceVersion + '</b></div>',
-        '<div>Firmware Type: <b>' + req.body.firmwareType + '</b></div>',
-        '<div style="margin-top:20px;">' + req.body.comments + '</div>'
-    ];
-    var html = markup.join('');
+  if (req.body.attachment) {
+    mailOptions.attachments = [
+      {
+        filename: 'error.log',
+        contents: req.body.attachment
+      }
+    ]
+  } else {
+    mailOptions.text += ' (No attachment was provided)'
+  }
 
-    var mailOptions = {
+  console.log('attempting to send email')
 
-        from: req.body.firstName, // sender address
-        to: 'alpinelabsemails@gmail.com', // list of receivers
-        subject: req.body.firstName + ' ' + '(' + req.body.email + ')', // Subject line
-        html: html
-    };
-
-    if (req.body.attachment) {
-        mailOptions.attachments = [{
-            filename: 'error.log',
-            contents: req.body.attachment
-        }];
+  // send mail with defined transport object
+  transporter1.sendMail(mailOptions, function(error, info) {
+    console.log('info is: ' + info)
+    if (error) {
+      console.log('we got an error' + error)
+      res.json('error', {
+        error: 'failed to send email'
+      })
     } else {
-        mailOptions.text += ' (No attachment was provided)';
+      console.log('bug report submitted successfully')
+      res.json({
+        success: true
+      })
+
+      sendEmailBackToReporter(req.body)
     }
-
-
-    console.log('attempting to send email');
-
-    // send mail with defined transport object
-    transporter1.sendMail(mailOptions, function(error, info) {
-        console.log('info is: ' + info);
-        if (error) {
-            console.log('we got an error' + error);
-            res.json('error', {
-                error: 'failed to send email'
-            });
-
-        } else {
-            console.log('bug report submitted successfully');
-            res.json({
-                success: true
-            });
-
-            sendEmailBackToReporter(req.body);
-        }
-        transporter1.close();
-    });
-});
+    transporter1.close()
+  })
+})
 
 function sendEmailBackToReporter(options) {
+  var fontPath = __dirname + '/../assets/fonts/'
+  var imagePath = __dirname + '/../assets/images/alpine-logo.png'
 
-    var fontPath = __dirname + '/../assets/fonts/';
-    var imagePath = __dirname + '/../assets/images/alpine-logo.png';
+  var html = template({
+    firstName: options.firstName,
+    fontPath: fontPath,
+    imagePath: imagePath
+  })
+  var plainText = plainTemplate({
+    firstName: options.firstName
+  })
+  var mailOptions = {
+    from: 'bug-reports@alpinelaboratories.com', // sender address
+    to: options.email, // list of receivers
+    subject: 'Thanks for reporting your issue', // Subject line
+    html: html,
+    text: plainText
+  }
 
+  console.log('sending email back to ' + options.email)
 
-    var html = template({
-        firstName: options.firstName,
-        fontPath: fontPath,
-        imagePath: imagePath
-    });
-    var plainText = plainTemplate({
-        firstName: options.firstName
-    });
-    var mailOptions = {
-
-        from: 'bug-reports@alpinelaboratories.com', // sender address
-        to: options.email, // list of receivers
-        subject: 'Thanks for reporting your issue', // Subject line
-        html: html,
-        text: plainText
-    };
-
-    console.log('sending email back to ' + options.email);
-
-
-    // send mail with defined transport object
-    transporter2.sendMail(mailOptions, function(error, info) {
-        console.log('info is: ' + info);
-        if (error) {
-            console.log('we got an error' + error);
-
-        } else {
-            console.log('Successfully sent email back to user');
-
-        }
-        transporter2.close();
-    });
+  // send mail with defined transport object
+  transporter2.sendMail(mailOptions, function(error, info) {
+    console.log('info is: ' + info)
+    if (error) {
+      console.log('we got an error' + error)
+    } else {
+      console.log('Successfully sent email back to user')
+    }
+    transporter2.close()
+  })
 }
 
-
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Express'
-    });
+  res.render('index', {
+    title: 'Express'
+  })
+})
 
-});
-
-module.exports = router;
+module.exports = router
